6fc7af1c5c5c6d2bd2af4fb10225fd094ae299c1
digi0ps <sriru1998@gmail.com>
2017-09-02 18:32:52 +0530

    Add favicons.

--- root/urls.py
+++ root/urls.py
@@ -5,7 +5,7 @@ from blog import urls, views
 
 # Regex to match every url except the ones we want Django to handle
 
-REGEX = r'^(?!.*(superuser|admin|favicon|static)).*$'
+REGEX = r'^(?!.*(superuser|admin|favicons|static)).*$'
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
