193a50296c1551a92d9a061bf97c5d20b5daf9fd
digi0ps <sriru1998@gmail.com>
2017-09-05 21:27:07 +0530

    Tweak allowed header.

--- root/settings.py
+++ root/settings.py
@@ -169,3 +169,14 @@ BLEACH_STRIP_TAGS = True
 # Cross Origin headers settings
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_METHODS = ('GET', 'POST')
+CORS_ALLOW_HEADERS = (
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+)
